a5f694e50bfc463a7fe38a831e5c8753186b41c7
Kenny Pflug <kenny.pflug@live.de>
2021-05-20 15:23:20 +0200

    introduced testsettings, German-specific tests are now disabled by default

--- Code/Synnotech.Time.Tests/CalculateIntervalForSameTimeNextDayTests.cs
+++ Code/Synnotech.Time.Tests/CalculateIntervalForSameTimeNextDayTests.cs
@@ -1,35 +1,45 @@
 ﻿using System;
-using System.Globalization;
 using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Synnotech.Xunit;
 using Xunit;
 
 namespace Synnotech.Time.Tests
 {
     public static class CalculateIntervalForSameTimeNextDayTests
     {
-        static CalculateIntervalForSameTimeNextDayTests()
-        {
-            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
-        }
-
         [Theory]
-        [MemberData(nameof(CalculateTimeSpansData))]
-        public static void CalculateTimeSpans(DateTime now, TimeSpan expected)
-        {
-            var startTime = new DateTime(1, 1, 1, 4, 15, 0, DateTimeKind.Local);
+        [MemberData(nameof(NonSpecificData))]
+        public static void CalculateTimeOfNextDayInNonSpecificCultureScenario(DateTime now, TimeSpan expected) =>
+            CheckTimeSpan(now, expected);
 
-            var actualTimeSpan = now.CalculateIntervalForSameTimeNextDay(startTime);
-
-            actualTimeSpan.Should().Be(expected);
-        }
-
-        public static readonly TheoryData<DateTime, TimeSpan> CalculateTimeSpansData =
+        public static readonly TheoryData<DateTime, TimeSpan> NonSpecificData =
             new ()
             {
                 { new DateTime(2017, 10, 4, 12, 0, 0, DateTimeKind.Local), new TimeSpan(16, 15, 0) }, // Simple example
                 { new DateTime(2016, 12, 31, 4, 15, 1, DateTimeKind.Local), new TimeSpan(23, 59, 59) }, // New Year's Eve
+            };
+
+        [SkippableTheory]
+        [MemberData(nameof(GermanSpecificData))]
+        public static void CalculateTimeOfNextDayInGermanScenario(DateTime now, TimeSpan expected)
+        {
+            Skip.IfNot(TestSettings.Configuration.GetValue<bool>("areGermanCultureSpecificTestsEnabled"));
+            CheckTimeSpan(now, expected);
+        }
+
+        public static readonly TheoryData<DateTime, TimeSpan> GermanSpecificData =
+            new ()
+            {
                 { new DateTime(2017, 03, 25, 18, 0, 0, DateTimeKind.Local), new TimeSpan(9, 15, 0) }, // Begin of Daylight Saving Time
                 { new DateTime(2017, 10, 28, 18, 0, 0, DateTimeKind.Local), new TimeSpan(11, 15, 0) } // End of Daylight Saving Time
             };
+
+        private static void CheckTimeSpan(DateTime now, TimeSpan expected)
+        {
+            var startTime = new DateTime(1, 1, 1, 4, 15, 0, DateTimeKind.Local);
+            var actualTimeSpan = now.CalculateIntervalForSameTimeNextDay(startTime);
+            actualTimeSpan.Should().Be(expected);
+        }
     }
 }
