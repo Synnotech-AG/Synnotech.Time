675023dcec6ac9828a1885aca68d561e5c5d9db1
Kenny Pflug <kenny.pflug@live.de>
2021-05-20 08:47:19 +0200

    added daily job

---
+++ Code/Synnotech.Time.Tests/CalculateIntervalForSameTimeNextDay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Synnotech.Time.Tests
+{
+    public static class CalculateIntervalForSameTimeNextDay
+    {
+        [Theory]
+        [MemberData(nameof(CalculateTimeSpansData))]
+        public static void CalculateTimeSpans(DateTime now, TimeSpan expected)
+        {
+            var startTime = new DateTime(1, 1, 1, 4, 15, 0, DateTimeKind.Local);
+
+            var actualTimeSpan = now.CalculateIntervalForSameTimeNextDay(startTime);
+
+            actualTimeSpan.Should().Be(expected);
+        }
+
+        public static readonly TheoryData<DateTime, TimeSpan> CalculateTimeSpansData =
+            new ()
+            {
+                { new DateTime(2017, 10, 4, 12, 0, 0, DateTimeKind.Local), new TimeSpan(16, 15, 0) }, // Simple example
+                { new DateTime(2016, 12, 31, 4, 15, 1, DateTimeKind.Local), new TimeSpan(23, 59, 59) }, // New Year's Eve
+                { new DateTime(2017, 03, 25, 18, 0, 0, DateTimeKind.Local), new TimeSpan(9, 15, 0) }, // Begin of Daylight Saving Time
+                { new DateTime(2017, 10, 28, 18, 0, 0, DateTimeKind.Local), new TimeSpan(11, 15, 0) } // End of Daylight Saving Time
+            };
+    }
+}
---
+++ Code/Synnotech.Time/TimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Synnotech.Time
+{
+    /// <summary>
+    /// Provides extension methods for time-related functionality.
+    /// </summary>
+    public static class TimeExtensions
+    {
+        /// <summary>
+        /// Calculates the time span from <paramref name="now" /> to the next day with the specified <paramref name="timeOfDay" />.
+        /// </summary>
+        /// <param name="now">The current time.</param>
+        /// <param name="timeOfDay">The target time of day of tomorrow. The date part of this value will be ignored.</param>
+        public static TimeSpan CalculateIntervalForSameTimeNextDay(this DateTime now, DateTime timeOfDay)
+        {
+            var tomorrow = now.AddDays(1.0);
+            var targetDateTime = new DateTime(tomorrow.Year, tomorrow.Month, tomorrow.Day, timeOfDay.Hour, timeOfDay.Minute, timeOfDay.Second, timeOfDay.Millisecond, timeOfDay.Kind);
+            return targetDateTime.ToUniversalTime() - now.ToUniversalTime();
+        }
+    }
+}
